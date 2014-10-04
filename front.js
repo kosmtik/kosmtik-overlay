347b624c27ed0935ea7d0097a7178759d491a220
Yohan Boniface <yb@enix.org>
2014-10-04 11:16:11 +0200

    Add keyboard shortcut

--- front.js
+++ front.js
@@ -17,21 +17,24 @@ L.K.Map.addInitHook(function () {
         title.innerHTML = 'Add an overlay';
         this.kosmtikOverlay = L.tileLayer(params.url, params);
         var builder = new L.K.FormBuilder(params, [
-            ['active', {handler: L.K.Switch, label: 'Active'}],
+            ['active', {handler: L.K.Switch, label: 'Active (alt+ctrl+O)'}],
             ['tms', {handler: L.K.Switch, label: 'TMS format.'}],
             ['url', {helpText: 'URL template.'}],
             ['opacity', {handler: 'FloatInput', helpText: 'Opacity: from 0 to 1 (opaque).'}],
             ['position', {handler: 'IntSelect', helpText: 'Position regarding to project\'s map.', selectOptions: [[1, 'Above'], [-1, 'Below']]}],
         ]);
+        var toggle = function () {
+            if (params.active) {
+                this.kosmtikOverlay.addTo(this);
+                updatePosition();
+            }
+            else {
+                this.removeLayer(this.kosmtikOverlay);
+            }
+        };
         builder.on('synced', function (e) {
             if (e.field === 'active') {
-                if (params.active) {
-                    this.kosmtikOverlay.addTo(this);
-                    updatePosition();
-                }
-                else {
-                    this.removeLayer(this.kosmtikOverlay);
-                }
+                L.bind(toggle, this)();
             } else if (e.field === 'url') {
                 this.kosmtikOverlay.setUrl(params.url);
             } else if (e.field === 'opacity') {
@@ -46,5 +49,18 @@ L.K.Map.addInitHook(function () {
             content: container
         });
         this.sidebar.rebuild();
+        var shortcutCallback = function () {
+            params.active = !params.active;
+            L.bind(toggle, this)();
+            builder.fetchAll();
+        };
+        this.shortcuts.add({
+            keyCode: L.K.Keys.O,
+            ctrlKey: true,
+            altKey: true,
+            callback: shortcutCallback,
+            context: this,
+            description: 'Toggle overlay'
+        });
     });
 });
