2ad07b404a0d5760d4c2dc1f5d2f734c3c7c2239
Hidde Wieringa <hidde@hiddewieringa.nl>
2020-09-12 19:22:45 +0200

    Use class syntax
    Ref https://github.com/kosmtik/kosmtik/pull/319

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-var patchConfig = function (e) {
-    e.options.overlay = e.project.mml.overlay || this.userConfig.overlay || {};
-    // Retrocompat
-    if (e.project.mml.overlayUrl) e.options.overlay.url = e.project.mml.overlayUrl;
-};
+class Overlay {
+    constructor(config) {
+        config.addJS('/node_modules/kosmtik-overlay/front.js');
+        config.on('project:tofront', this.patchConfig);    
+    }
 
-exports.Plugin = function (config) {
-    config.addJS('/node_modules/kosmtik-overlay/front.js');
-    config.on('project:tofront', patchConfig);
-};
+    patchConfig(e) {
+        e.options.overlay = e.project.mml.overlay || this.userConfig.overlay || {};
+        // Retrocompat
+        if (e.project.mml.overlayUrl) e.options.overlay.url = e.project.mml.overlayUrl;
+    };
+}
+
+exports = { Plugin: Overlay }
